386244118e695d1c0659cca002cc501b17760c79
unknown <abdulmukti40390@gmail.comgit config --global.user.mukti>
2025-02-25 12:03:08 +0700

    fix again

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import Hero from "@/components/Hero";
 import Service from "@/components/Service";
 import Testimoni from "@/components/ui/Testimoni";
-import FAQ from "@/components/Faq";
 import Journals from "@/components/Journals";
 import { Projects } from "@/components/Projects";
+import FAQ from "@/components/FAQ";
 
 function Home() {
   return (
